d8806676f36837ed6533bba0ea920665689fe153
Mohammad H Forouhesh <Mohammad.Forouhesh@outlook.com>
2017-01-12 17:23:51 +0330

    listify bug fixed

--- StaticMethod.py
+++ StaticMethod.py
@@ -3,5 +3,7 @@ def listify(obj):
     if obj is None:
         return []
     else:
-        return obj if isinstance(obj, (list, tuple, type(None)))
+        return obj if isinstance(obj, (list, tuple, type(None)))else [obj]
+
+
 
