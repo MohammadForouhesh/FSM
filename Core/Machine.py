9b255159049be3de7819bf4ec0606a04c3b5c098
Mohammad H Forouhesh <Mohammad.Forouhesh@outlook.com>
2017-01-14 00:12:40 +0330

    root out dynamic typing system

--- Core/Machine.py
+++ Core/Machine.py
@@ -21,41 +21,12 @@ class Machine(object):
     __slots__ = 'models', 'states', '_initial', 'send_event','ignore_invalid_triggers',\
                 'name', '_queued', '_transition_queue', '_initial',\
                 'events', 'id',
-                
-    # Callback naming parameters
-    callbacks = ['before', 'after', 'prepare', 'on_enter', 'on_exit']
+
     separator = '_'
 
-    def __init__(self, model: object, states: State=None, initial:str=None, transitions: list=None,
+    def __init__(self, states: State=None, initial:str=None, transitions: list=None,
                  send_event: bool=False, ignore_invalid_triggers: bool=None, name: str=None,
                  queued: bool=False):
-        """
-            :param model                  : The object(s) whose states we want to manage. If None,
-                                            the current Machine instance will be used the model (i.e., all
-                                            triggering events will be attached to the Machine itself).
-            :param states                 : A list of valid states. Each element can be either a
-                                            string or a State instance. If string, a new generic State
-                                            instance will be created that has the same name as the string.
-            :param initial                : The initial state of the Machine.
-            :param transitions            : An optional list of transitions. Each element
-                                            is a dictionary of named arguments to be passed onto the
-                                            Transition initializer.
-            :param send_event             : When True, any arguments passed to trigger
-                                            methods will be wrapped in an EventData object, allowing
-                                            indirect and encapsulated access to data. When False, all
-                                            positional and keyword arguments will be passed directly to all
-                                            callback methods.
-            :param ignore_invalid_triggers: when True, any calls to trigger methods
-                                            that are not valid for the present state (e.g., calling an
-                                            a_to_b() trigger when the current state is c) will be silently
-                                            ignored rather than raising an invalid transition exception.
-            :param name                   : If a name is set, it will be used as a prefix for logger output
-            :param queued                 : When True, processes transitions sequentially. A trigger
-                                            executed in a state callback function will be queued and executed 
-                                            later. Due to the nature of the queued processing, all transitions will
-                                            _always_ return True since conditional checks cannot be conducted at 
-                                            queueing time.
-        """
 
         try:
             super(Machine, self).__init__()
@@ -69,9 +40,8 @@ class Machine(object):
         self.id = name + ": " if name is not None else ""
         self._queued = queued
         self._transition_queue = deque()
-        self.models = []
 
-        if model and initial is None:
+        if initial is None:
             initial = 'initial'
             self.add_states(initial)
         self._initial = initial
@@ -86,42 +56,6 @@ class Machine(object):
                     self.add_transition(*t)
                 else:
                     self.add_transition(**t)
-
-        if model:
-            self.add_model(model)
-
-    def add_model(self, model, initial=None):
-        """ Register a model with the state machine, initializing triggers and callbacks. """
-        models = listify(model)
-
-        if initial is None:
-            if self._initial is None:
-                raise MachineError("No initial state configured for machine, must specify when adding model.")
-            else:
-                initial = self._initial
-
-        for model in models:
-            if model not in self.models:
-
-                if hasattr(model, 'trigger'):
-                    logger.warning("%sModel already contains an attribute 'trigger'. Skip method binding ",self.id)
-                else:
-                    model.trigger = partial(get_trigger, model)
-
-                for trigger, _ in self.events.items():
-                    self._add_trigger_to_model(trigger, model)
-
-                for _, state in self.states.items():
-                    self._add_model_to_state(state, model)
-
-                self.set_state(initial, model=model)
-                self.models.append(model)
-
-    def remove_model(self, model):
-        models = listify(model)
-
-        for model in models:
-            self.models.remove(model)
 
     @staticmethod
     def _create_transition(*args, **kwargs):
@@ -141,16 +75,9 @@ class Machine(object):
         """ Return boolean indicating if machine has queue or not """
         return self._queued
 
-    @property
-    def model(self):
-        if len(self.models) == 1:
-            return self.models[0]
-        else:
-            return self.models
-
-    def is_state(self, state, model):
+    def is_state(self, state):
         """ Check whether the current state matches the named state. """
-        return model.state == state
+        return state in self.states
 
     def get_state(self, state):
         """ Return the State instance with the passed name. """
@@ -158,39 +85,13 @@ class Machine(object):
             raise ValueError("State '%s' is not a registered state." % state)
         return self.states[state]
 
-    def set_state(self, state, model=None):
+    def set_state(self, state):
         """ Set the current state. """
         if isinstance(state, string_types):
             state = self.get_state(state)
-        models = self.models if model is None else listify(model)
-        for m in models:
-            m.state = state.name
-
-    def add_state(self, *args, **kwargs):
-        """ Alias for add_states. """
-        self.add_states(*args, **kwargs)
 
     def add_states(self, states: (list or str or dict or State), on_enter: (str or list)=None,
                    on_exit: (str or list)=None, ignore_invalid_triggers: bool=None):
-        """ Add new state(s).
-            :param states                   : a list, a State instance, the
-                                              name of a new state, or a dict with keywords to pass on to the
-                                              State initializer. If a list, each element can be of any of the
-                                              latter three types.
-            :param on_enter                 : callbacks to trigger when the state is
-                                              entered. Only valid if first argument is string.
-            :param on_exit                  : callbacks to trigger when the state is
-                                              exited. Only valid if first argument is string.
-            :param ignore_invalid_triggers  : when True, any calls to trigger methods
-                                              that are not valid for the present state (e.g., calling an
-                                              a_to_b() trigger when the current state is c) will be silently
-                                              ignored rather than raising an invalid transition exception.
-                                              Note that this argument takes precedence over the same
-                                              argument defined at the Machine level, and is in turn
-                                              overridden by any ignore_invalid_triggers explicitly
-                                              passed in an individual state's initialization arguments.
-        """
-
         ignore = ignore_invalid_triggers
         if ignore is None:
             ignore = self.ignore_invalid_triggers
@@ -204,51 +105,14 @@ class Machine(object):
                     state['ignore_invalid_triggers'] = ignore
                 state = State.State(**state)
             self.states[state.name] = state
-            for model in self.models:
-                self._add_model_to_state(state, model)
-        # Add automatic transitions after all states have been created
-
-    def _add_model_to_state(self, state, model):
-        setattr(model, 'is_%s' % state.name,
-                partial(self.is_state, state.name, model))
-        #  Add enter/exit callbacks if there are existing bound methods
-        enter_callback = 'on_enter_' + state.name
-        if hasattr(model, enter_callback) and \
-                inspect.ismethod(getattr(model, enter_callback)):
-            state.add_callback('enter', enter_callback)
-        exit_callback = 'on_exit_' + state.name
-        if hasattr(model, exit_callback) and \
-                inspect.ismethod(getattr(model, exit_callback)):
-            state.add_callback('exit', exit_callback)
-
-    def _add_trigger_to_model(self, trigger, model):
-        trig_func = partial(self.events[trigger].trigger, model)
-        setattr(model, trigger, trig_func)
 
     def get_triggers(self, *args):
         states = set(args)
         return [t for (t, ev) in self.events.items() if any(state in ev.transitions for state in states)]
 
     def add_transition(self, trigger: str, source: str, dest: str, conditions:(str or list)=None):
-        """ Create a new Transition instance and add it to the internal list.
-
-            :param trigger    : The name of the method that will trigger the
-                                transition. This will be attached to the currently specified
-                                model (e.g., passing trigger='advance' will create a new
-                                advance() method in the model that triggers the transition.)
-            :param source     : The name of the source state--i.e., the state we
-                                are transitioning away from.
-            :param dest       : The name of the destination State--i.e., the state
-                                we are transitioning into.
-            :param conditions : Condition(s) that must pass in order
-                                for the transition to take place. Either a list providing the
-                                name of a callable, or a list of callables. For the transition
-                                to occur, ALL callables must return True.
-        """
         if trigger not in self.events:
             self.events[trigger] = self._create_event(trigger, self)
-            for model in self.models:
-                self._add_trigger_to_model(trigger, model)
 
         if isinstance(source, string_types):
             source = list(self.states.keys()) if source == '*' else [source]
@@ -259,21 +123,6 @@ class Machine(object):
                 dest = dest.name
             t = self._create_transition(s, dest, conditions)
             self.events[trigger].add_transition(t)
-
-    def _callback(self, func: callable, event_data: EventData):
-        """ Trigger a callback function, possibly wrapping it in an EventData instance.
-            func        : The callback function.
-            event_data  : An EventData instance to pass to the
-                          callback (if event sending is enabled) or to extract arguments
-                          from (if event sending is disabled).
-        """
-        if isinstance(func, string_types):
-            func = getattr(event_data.model, func)
-
-        if self.send_event:
-            func(event_data)
-        else:
-            func(*event_data.args, **event_data.kwargs)
 
     def _has_state(self, s):
         if isinstance(s, State.State):
@@ -311,41 +160,9 @@ class Machine(object):
                 raise
         return True
 
-    @classmethod
-    def _identify_callback(cls, name):
-        try:
-            callback_type = cls.callbacks[[name.find(x) for x in cls.callbacks].index(0)]
-        except ValueError:
-            return None, None
 
-        # Extract the target by cutting the string after the type and separator
-        target = name[len(callback_type) + len(cls.separator):]
-
-        # Make sure there is actually a target to avoid index error and enforce _ as a separator
-        if target == '' or name[len(callback_type)] != cls.separator:
-            return None, None
-
-        return callback_type, target
-
-    def __getattr__(self, name):
-        if name.startswith('__'):
-            raise AttributeError("{} does not exist".format(name))
-
-        # Could be a callback
-        callback_type, target = self._identify_callback(name)
-
-        if callback_type is not None:
-            if callback_type in ['before', 'after', 'prepare']:
-                if target not in self.events:
-                    raise MachineError('Event "%s" is not registered.' % target)
-                return partial(self.events[target].add_callback, callback_type)
-
-            elif callback_type in ['on_enter', 'on_exit']:
-                state = self.get_state(target)
-                return partial(state.add_callback, callback_type[3:])
-
-        # Nothing matched
-        raise AttributeError("{} does not exist".format(name))
+        # 3123123
+# 123123
 
 
 class MachineError(Exception):
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os, sys, inspect
 from pprint import pprint
 
+from IPython.core.display import display, Image
+
 from Extensions.Diagrams.GraphMachine import GraphMachine
 cmd_folder = os.path.realpath(
     os.path.dirname(
@@ -9,50 +11,33 @@ if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
 
-class Matter(object):
-    def is_valid(self):
-        return True
-
-    def is_not_valid(self):
-        return False
-
-    def is_also_valid(self):
-        return True
-
-    def satisfaction(self, stream: str) -> bool:
-        pass
-
-    # graph object is created by the machine
-    def show_graph(self, name: str):
-        self.graph.draw('state' + name + '.png', prog='dot')
-
-
-states=['000', '001', '010', '011', '100', '101', '110', '111']
+states=['00', '01', '10', '11']
 transitions = [
-    {'trigger': 'add_zero', 'source': '000', 'dest': '100'},
-    {'trigger': 'add_one', 'source': '000', 'dest': '001'},
-    {'trigger': 'add_zero', 'source': '001', 'dest': '101'},
-    {'trigger': 'add_one', 'source': '001', 'dest': '010'},
-    {'trigger': 'add_zero', 'source': '010', 'dest': '110'},
-    {'trigger': 'add_one', 'source': '010', 'dest': '011'},
-    {'trigger': 'add_zero', 'source': '011', 'dest': '111'},
-    {'trigger': 'add_one', 'source': '011', 'dest': '000'},
-    {'trigger': 'add_zero', 'source': '100', 'dest': '000'},
-    {'trigger': 'add_one', 'source': '100', 'dest': '101'},
-    {'trigger': 'add_zero', 'source': '101', 'dest': '001'},
-    {'trigger': 'add_one', 'source': '101', 'dest': '110'},
-    {'trigger': 'add_zero', 'source': '110', 'dest': '010'},
-    {'trigger': 'add_one', 'source': '110', 'dest': '111'},
-    {'trigger': 'add_zero', 'source': '111', 'dest': '011'},
-    {'trigger': 'add_one', 'source': '111', 'dest': '100'},
+    {'trigger': 'darja', 'source': '00', 'dest': '00'},
+    {'trigger': 'add_one', 'source': '00', 'dest': '01'},
+    {'trigger': 'add_two', 'source': '00', 'dest': '10'},
+    {'trigger': 'darja', 'source': '01', 'dest': '01'},
+    {'trigger': 'add_one', 'source': '01', 'dest': '10'},
+    {'trigger': 'add_two', 'source': '01', 'dest': '11'},
+    {'trigger': 'darja', 'source': '10', 'dest': '10'},
+    {'trigger': 'add_one', 'source': '10', 'dest': '11'},
+    {'trigger': 'add_two', 'source': '10', 'dest': '00'},
+    {'trigger': 'darja', 'source': '11', 'dest': '11'},
+    {'trigger': 'add_one', 'source': '11', 'dest': '00'},
+    {'trigger': 'add_two', 'source': '11', 'dest': '01'}
 ]
 
-model = Matter()
-machine = GraphMachine(model=model,
-                       states=states,
+machine = GraphMachine(states=states,
                        transitions=transitions,
-                       initial='000',
+                       initial='00',
                        title="transient",
                        show_conditions=True)
 
-print(machine.is_state('000', model=model))
+machine.graph.draw('state.png', prog='dot')
+display(Image('state.png'))
+
+print(machine.events)
+
+print(machine.darja())
+
+print(machine.is_state('00'))
